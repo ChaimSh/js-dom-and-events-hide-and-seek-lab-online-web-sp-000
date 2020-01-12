b951e265704fd3ed6cb430bfbfabd1841e9c4c05
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-01-12 17:31:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,9 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  return document.querySelectorAll('.ranked-list ')
+  const rankedLists = document.querySelectorAll('.ranked-list ')
+
+  for (let i = 0, i < rankedLists.length, i++) {
+    rankedLists[i].innerHTML =(i + 1).toString()
+  }
 }
